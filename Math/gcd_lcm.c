b8e8ddc7f7cc739622b0dae02f55f175ece1062d
Soumyajit <87481763+soumyajit0@users.noreply.github.com>
2022-12-13 14:11:38 +0530

    Add files via upload

---
+++ Math/gcd_lcm.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int gcd(int a,int b)
+{
+    if(b==0)
+    {
+        return a;
+    }
+    return gcd(b,a%b);
+}
+
+int lcm(int a,int b,int hcf)
+{
+    return a*b/hcf;
+}
+
+int main()
+{
+    int a,b;
+    printf("Enter the first no. : ");
+    scanf("%d",&a);
+    printf("Enter the second no. : ");
+    scanf("%d",&b);
+
+    int hcf=gcd(a,b);
+    printf("The GCD is : %d\n",hcf);
+    printf("The LCM is : %d\n",lcm(a,b,hcf));
+
+    return 0;
+}
