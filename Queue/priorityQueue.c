89f1f5c305a63dd539f02b51869af6241e7cd5a8
Soumyajit <87481763+soumyajit0@users.noreply.github.com>
2022-12-17 20:44:34 +0530

    Update priorityQueue.c

--- Queue/priorityQueue.c
+++ Queue/priorityQueue.c
@@ -1,54 +1,48 @@
 #include<stdio.h>
-#include<stdbool.h>
 #include<stdlib.h>
+#include<stdbool.h>
 
 struct node{
     int data;
     int priority;
-    struct node *next;;
+    struct node *next;
 };
 
 struct node * enqueue(struct node *head){
-    int value,pr;
-    printf("Enter the value to be inserted : ");
+    int value,power;
+    printf("\nEnter the Value to be enqueue : ");
     scanf("%d",&value);
-    printf("Enter the priority : ");
-    scanf("%d",&pr);
-    printf("\n%d is inserted in the Queue with %d priority\n",value,pr);
+    printf("Enter the priority of the value : ");
+    scanf("%d",&power);
     struct node *temp=(struct node *)malloc(sizeof(struct node));
     temp->data=value;
-    temp->priority=pr;
-    temp->next=NULL;
-    if (head==NULL || head->priority<pr){
+    temp->priority=power;
+    printf("%d is enqueued in the Priority Queue with %d priority\n",value,power);
+    if (head==NULL || head->priority<power){
         temp->next=head;
         return temp;
     }
     struct node *ptr=head;
-    while(ptr->next!=NULL){
-        if (ptr->next->priority<pr){
-            temp->next=ptr->next;
-            ptr->next=temp;
-            return head;
-        }
+    while(ptr->next!=NULL && ptr->next->priority>power)
         ptr=ptr->next;
-    }
+    temp->next=ptr->next;
     ptr->next=temp;
     return head;
 }
 
 struct node * dequeue(struct node *head){
     if (head==NULL){
-        printf("\nUnderflow\n");
+        printf("Priority Queue Underflow\n");
         return head;
     }
-    printf("\n%d is dequeued\n",head->data);
     struct node *temp=head->next;
+    printf("%d is dequeued from the Priority Queue\n",head->data);
     free(head);
     return temp;
 }
 
 void display(struct node *head){
-    printf("\nThe Priority Queue is : ");
+    printf("\nPriority Queue : \n");
     while(head!=NULL){
         printf("%d ",head->data);
         head=head->next;
@@ -57,8 +51,8 @@ void display(struct node *head){
 }
 
 int main(){
-    struct node *head=NULL;
     int c;
+    struct node *head=NULL;
     while(true){
         printf("\nPress 1 to Enqueue\n");
         printf("Press 2 to Dequeue\n");
