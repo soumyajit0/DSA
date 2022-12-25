1ff45e344e2a957c590db50fdab3decedcbd9269
Sonika12Biswas <118993650+Sonika12Biswas@users.noreply.github.com>
2022-12-26 02:56:10 +0530

    Create pascal'sTriangle.cpp

---
+++ Pattern/pascal'sTriangle.cpp
@@ -0,0 +1,23 @@
+//pattern
+//        1
+//      1   1
+//     1   2   1
+//  1   3   3   1
+//1   4   6   4   1
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int num=1;
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        for(int k=0;k<=i;k++){
+            cout<<num<<" ";
+            num=num*(i-k)/(k+1);
+        }
+        cout<<endl;
+    }
+}
