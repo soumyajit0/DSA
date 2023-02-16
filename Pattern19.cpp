db79b999be3cf1aa91a0e0e759c882db7e28f62a
Prathama Pratihar <120256509+Prathama04@users.noreply.github.com>
2023-02-16 19:11:34 +0530

    Create Pattern19.cpp

---
+++ Pattern19.cpp
@@ -0,0 +1,55 @@
+/*Pattern19
+**********
+****  ****
+***    ***
+**      **
+*        *
+*        *
+**      ** 
+***    ***
+****  ****
+**********
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n=5;
+    // n stores the number of rows in the pattern
+for (int i=1;i<=n;i++) // i denotes the row no. of the pattern
+    {
+        //STARS
+        for (int j=1;j<=n-(i-1);j++) // j denotes the column no. of the pattern
+        {
+         cout<<"*";
+        }
+        //SPACE
+        for (int j=1;j<=(2*i-2);j++){
+        cout << " ";
+        }
+        //STARS
+        for (int j=1;j<=(n-(i-1));j++) 
+        {
+         cout<<"*";
+        }
+        cout<< "\n";
+    }
+for (int i=1;i<=n;i++) 
+    {
+        //STARS
+        for (int j=1;j<=i;j++) 
+        {
+         cout<<"*";
+        }
+        //SPACE
+        for (int j=1;j<=(2*n-2*i);j++){
+        cout << " ";
+        }
+        //STARS
+        for (int j=1;j<=i;j++) 
+        {
+         cout<<"*";
+        }
+        cout<<"\n";
+     }
+return 0;
+}
