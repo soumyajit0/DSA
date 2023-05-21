9cbdfdc63f2f149a316f75040d1d2a7b0c1419b7
soumyajit0 <soumyajitdeysarkar@gmail.com>
2023-05-21 12:51:07 +0530

    Codes

--- Recursion/MCM.cpp
+++ Recursion/MCM.cpp
@@ -1,18 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int MCM(vector<int> arr, int i, int j, vector<vector<int>> dp)
+int MCM(vector<int> arr, int i, int j)
 {
     if (i + 1 == j)
         return 0;
-    if (dp[i][j] != -1)
-        return dp[i][j];
     int res = INT_MAX;
     for (int k = i + 1; k < j; k++)
     {
-        res = min(res, MCM(arr, i, k, dp) + MCM(arr, k, j, dp) + arr[i] * arr[k] * arr[j]);
+        res = min(res, MCM(arr, i, k) + MCM(arr, k, j) + arr[i] * arr[k] * arr[j]);
     }
-    dp[i][j] = res;
     return res;
 }
 
@@ -22,12 +19,11 @@ int main()
     cout << "Enter the no. of Matrices : ";
     cin >> n;
     vector<int> arr(n + 1);
-    vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
     cout << "Enter the Dimensions of the Matrices : ";
     for (int i = 0; i <= n; i++)
     {
         cin >> arr[i];
     }
-    cout << "The Minimum No. of Operations : " << MCM(arr, 0, n, dp) << endl;
+    cout << "The Minimum No. of Operations : " << MCM(arr, 0, n) << endl;
     return 0;
 }
